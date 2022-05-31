1b408225f0cdf74e4768e654357a81aeb04400c7
expoilderk <falecommarlonreis@gmail.com>
2022-05-30 23:39:13 -0300

    Create the first project files

---
+++ get_next_line.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mreis-me <falecommarlonreis@gmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 19:27:26 by mreis-me          #+#    #+#             */
+/*   Updated: 2022/05/30 23:37:48 by mreis-me         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	char	*buffer;
+	char	*cur_line;
+	static char	*next_line;
+	int			count;
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mreis-me <falecommarlonreis@gmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 19:27:38 by mreis-me          #+#    #+#             */
+/*   Updated: 2022/05/30 19:40:34 by mreis-me         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE.H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+
+char	*get_next_line(int fd);
+
+#endif
+
+
